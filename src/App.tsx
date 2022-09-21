9ff7a53d59c69896624deaefeba88e73b361a2ee
Lars Flieger <a.purenetwork@gmail.com>
2022-09-21 23:09:33 +0200

    Add base

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Create } from "./pages/Create";
+import { Join } from "./pages/Join";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+    <h2>Welcome!</h2>
+    <Create />
+    <Join />
+  </>
 }
 
 export default App;
